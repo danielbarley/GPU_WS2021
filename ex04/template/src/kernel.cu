d8885f74e2ca8ece63fad34623c09fada1d4253c
Benjamin Maier <benjamin.maier@hexagon.com>
2020-11-30 16:15:54 +0100

    update global2shared

--- ex04/template/src/kernel.cu
+++ ex04/template/src/kernel.cu
@@ -20,11 +20,9 @@ globalMem2SharedMem
 {
 	extern __shared__ float shm[];
 	// assume one dimensional blocks
-	const size_t blocks = gridDim.x;
 	const size_t threads = gridDim.x * blockDim.x;
 	const size_t threadsPerBlock = blockDim.x;
 	const size_t elementsPerThread = (n + threads - 1) / threads;
-	//const size_t elementsPerBlock = (n + blocks - 1) / blocks;
 
 	int startIndex = (threadIdx.x + blockIdx.x * blockDim.x) * elementsPerThread;
 	for (int i = 0; i < elementsPerThread; i += threadsPerBlock)
@@ -71,18 +69,16 @@ SharedMem2globalMem
 {
 	extern __shared__ float shm[];
 	// assume one dimensional blocks
-	const size_t blocks = gridDim.x;
 	const size_t threads = gridDim.x * blockDim.x;
 	const size_t threadsPerBlock = blockDim.x;
 	const size_t elementsPerThread = (n + threads - 1) / threads;
-	//const size_t elementsPerBlock = (n + blocks - 1) / blocks;
 
 	int startIndex = (threadIdx.x + blockIdx.x * blockDim.x) * elementsPerThread;
 	for (int i = 0; i < elementsPerThread; i += threadsPerBlock)
 	{
 		const size_t index = startIndex + i;
 		if (i < n)
-			data[index] = shm[index];
+			data[0] = shm[index];
 	}
 }
 void SharedMem2globalMem_Wrapper(dim3 gridSize, dim3 blockSize, int shmSize, float* data) {
@@ -96,11 +92,9 @@ SharedMem2Registers
 {
 	extern __shared__ float shm[];
 	// assume one dimensional blocks
-	const size_t blocks = gridDim.x;
 	const size_t threads = gridDim.x * blockDim.x;
 	const size_t threadsPerBlock = blockDim.x;
 	const size_t elementsPerThread = (n + threads - 1) / threads;
-	//const size_t elementsPerBlock = (n + blocks - 1) / blocks;
 
 	int startIndex = (threadIdx.x + blockIdx.x * blockDim.x) * elementsPerThread;
 	float reg;
@@ -125,11 +119,9 @@ Registers2SharedMem
 {
 	extern __shared__ float shm[];
 	// assume one dimensional blocks
-	const size_t blocks = gridDim.x;
 	const size_t threads = gridDim.x * blockDim.x;
 	const size_t threadsPerBlock = blockDim.x;
 	const size_t elementsPerThread = (n + threads - 1) / threads;
-	//const size_t elementsPerBlock = (n + blocks - 1) / blocks;
 
 	int startIndex = (threadIdx.x + blockIdx.x * blockDim.x) * elementsPerThread;
 	float reg = 133.7;
@@ -150,12 +142,30 @@ void Registers2SharedMem_Wrapper(dim3 gridSize, dim3 blockSize, int shmSize, flo
 
 __global__ void 
 bankConflictsRead
-//(/*TODO Parameters*/)
-( )
+(float* data, size_t n, size_t stride, long* d_clock)
 {
-	/*TODO Kernel Code*/
+	extern __shared__ float shm[];
+	// assume one dimensional blocks
+	size_t index = (threadIdx.x + blockIdx.x * blockDim.x) * stride;
+	// "modulo"
+	size_t q = index / n;
+	size_t p = q * n;
+	index -= p;
+	float reg;
+	int iterations = 100;
+	long start = clock64();
+	for (int i = 0; i < iterations; i++)
+	{
+		reg = shm[index];
+	}
+
+	(*d_clock) += clock64() / iterations - start / iterations;
+
+	if (index == 0)
+		data[0] = reg;
 }
 
-void bankConflictsRead_Wrapper(dim3 gridSize, dim3 blockSize, int shmSize /* TODO Parameters*/) {
-	//globalMem2SharedMem<<< gridSize, blockSize, shmSize >>>( /* TODO Parameters */);
+void bankConflictsRead_Wrapper(dim3 gridSize, dim3 blockSize, int shmSize, float* data, size_t stride, long* d_clock) {
+	size_t n = shmSize / sizeof(float);
+	bankConflictsRead<<< gridSize, blockSize, shmSize >>>(data, n, stride, d_clock);
 }
